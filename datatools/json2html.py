c694843cb897423ecf0c28449e7784aaa646e8a9
semicontinuity <igor.a.karpov@gmail.com>
2021-03-10 14:04:03 +0300

    json2html: html template

--- datatools/json2html.py
+++ datatools/json2html.py
@@ -3,6 +3,7 @@ import json
 import os
 import sys
 from json import JSONDecodeError
+from string import Template
 
 from datatools.json.coloring import *
 from datatools.json.structure_analyzer import *
@@ -39,82 +40,97 @@ def stderr_print(*args, **kwargs):
 
 
 class PageNode:
+
+    html_template = Template("""<html lang="en">
+<head>
+<title>$title</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<link rel="icon" href="data:,">
+
+<style>
+body {font-family: monospace; display: inline-block;}
+main {display: inline-flex; border-left: solid 2px darkgrey; border-right: solid 2px darkgrey;}
+thead {border: solid 1px darkgray;}
+table {border-collapse: collapse; padding: 0;}
+//table.ov { width:100%; }
+//.a { width: 100%;}
+.ae { display: inline-block;}
+.index {border: solid 1px darkcyan; color: darkcyan;}
+div.regular>span.header    {display: none;}
+div.collapsed2>span.header {display: block; font-weight: bold; background: lightgray; border: solid 1px darkgray;}
+div.collapsed2>span.ae {display: none;}
+div.collapsed2>table {display: none;}
+tr.collapsed2>td {display: none;}
+.none {background: darkgray;}
+span { white-space: pre;}
+//td {border: solid 1px #CCC; padding-left: 0.25em; padding-right: 0.25em;}
+th.a {background: lightgray; }
+table.aon th {border: solid 1px darkgrey; }
+table.aon td {border: solid 1px darkgrey; }
+table.aohwno th {border: solid 1px darkgrey; }
+table.aohwno td {border: solid 1px darkgrey; }
+td {border-top: solid 1px #CCC; border-bottom: solid 1px #CCC; padding: 0;}
+th {border-top: solid 1px darkgrey; border-bottom: solid 1px darkgrey; background: #DDD}
+th.ov_th {border-right: solid 2px darkgrey; }
+//tr:nth-child(odd)  td.index {background: #CCC;}
+//tr:nth-child(even) td.index {background: #BBB;}
+//td.a_v {width:100%;}
+//td.ov_v {width:100%;}
+
+.int {color: darkred; padding-left: 0.25em; padding-right: 0.25em;}
+.float {color: darkred; padding-left: 0.25em; padding-right: 0.25em;}
+.str {color: navy; padding-left: 0.25em; padding-right: 0.25em;}
+.bool {color: darkgreen; padding-left: 0.25em; padding-right: 0.25em;}
+
+.collapsed {display: none;}
+</style>
+
+<script>
+function toggleClass(element, className) {
+  const classes = element.classList;
+  if (classes.contains(className)) {
+     classes.remove(className);
+  } else {
+     classes.add(className);
+  }
+}
+function toggleClass2(element, class1, class2) {
+  const classes = element.classList;
+  if (classes.contains(class1)) {
+     classes.remove(class1);
+     classes.add(class2);
+  } else {
+     classes.remove(class2);
+     classes.add(class1);
+  }
+}
+function toggle(e) {
+  const tb = e.parentElement.parentElement.parentElement.getElementsByTagName("tbody")[0];
+  toggleClass(tb, "collapsed");
+}
+function toggle2(e, tagName) {
+  while (e.tagName !== tagName) e = e.parentElement;
+  toggleClass2(e, "collapsed2", "regular");
+}
+</script>
+
+</head>
+
+<body><main>
+
+
+$view
+
+
+</main></body>
+</html>""")
+
     def __init__(self, root, title):
         self.root = root
         self.title = title
 
     def __str__(self):
-        return f'<html>\n' + \
-               '<head>\n' + \
-               f'<title>{self.title}</title>\n' + \
-               '<meta http-equiv="Content-Type" content="text/html; charset=utf-8">\n' + \
-               '<link rel="icon" href="data:,">\n' + \
-               '<style>\n' + \
-               'body {font-family: monospace; display: inline-block;}\n' + \
-               'main {display: inline-flex; border-left: solid 2px darkgrey; border-right: solid 2px darkgrey;}\n' + \
-               'thead {border: solid 1px darkgray;}\n' + \
-               'table {border-collapse: collapse; padding: 0;}\n' + \
-               '//table.ov { width:100%; }\n' + \
-               '//.a { width: 100%;}\n' + \
-               '.ae { display: inline-block;}\n' + \
-               '.index {border: solid 1px darkcyan; color: darkcyan;}\n' + \
-               'div.regular>span.header    {display: none;}\n' + \
-               'div.collapsed2>span.header {display: block; font-weight: bold; background: lightgray; border: solid 1px darkgray;}\n' + \
-               'div.collapsed2>span.ae {display: none;}\n' + \
-               'div.collapsed2>table {display: none;}\n' + \
-               'tr.collapsed2>td {display: none;}\n' + \
-               '.none {background: darkgray;}\n' + \
-               'span {padding-left: 0.25em; padding-right: 0.25em; white-space-pre;}\n' + \
-               '//td {border: solid 1px #CCC; padding-left: 0.25em; padding-right: 0.25em;}\n' + \
-               'th.a {background: lightgray; }\n' + \
-               'table.aon th {border: solid 1px darkgrey; }\n' + \
-               'table.aon td {border: solid 1px darkgrey; }\n' + \
-               'table.aohwno th {border: solid 1px darkgrey; }\n' + \
-               'table.aohwno td {border: solid 1px darkgrey; }\n' + \
-               'td {border-top: solid 1px #CCC; border-bottom: solid 1px #CCC; padding: 0;}\n' + \
-               'th {border-top: solid 1px darkgrey; border-bottom: solid 1px darkgrey; background: #DDD}\n' + \
-               'th.ov_th {border-right: solid 2px darkgrey; }\n' + \
-               '//tr:nth-child(odd)  td.index {background: #CCC;}\n' + \
-               '//tr:nth-child(even) td.index {background: #BBB;}\n' + \
-               '//td.a_v {width:100%;}\n' + \
-               '//td.ov_v {width:100%;}\n' + \
-               '.int {color: darkred;}\n' + \
-               '.float {color: darkred;}\n' + \
-               '.str {color: navy;}\n' + \
-               '.bool {color: darkgreen;}\n' + \
-               '.collapsed {display: none;}\n' + \
-               '</style>\n' + \
-               '<script>\n' + \
-               '  function toggleClass(element, className) {' + \
-               '    const classes = element.classList;' + \
-               '    if (classes.contains(className)) {' + \
-               '        classes.remove(className);' + \
-               '    } else {' + \
-               '        classes.add(className);' + \
-               '    }' + \
-               '  }' + \
-               '  function toggleClass2(element, class1, class2) {' + \
-               '    const classes = element.classList;' + \
-               '    if (classes.contains(class1)) {' + \
-               '        classes.remove(class1);' + \
-               '        classes.add(class2);' + \
-               '    } else {' + \
-               '        classes.remove(class2);' + \
-               '        classes.add(class1);' + \
-               '    }' + \
-               '  }' + \
-               '  function toggle(e) { ' \
-               '    const tb = e.parentElement.parentElement.parentElement.getElementsByTagName("tbody")[0];' \
-               '    toggleClass(tb, "collapsed");' \
-               '  }\n' + \
-               '  function toggle2(e, tagName) { ' \
-               '    while (e.tagName !== tagName) e = e.parentElement;' \
-               '    toggleClass2(e, "collapsed2", "regular");' \
-               '  }\n' + \
-               '</script>\n' + \
-               '</head>\n' + \
-               f'<body><main>\n\n{str(self.root)}\n\n</main></body>\n' \
-               '</html>\n'
+        return self.html_template.substitute(title=self.title, view=str(self.root))
 
 
 class MatrixNode:
