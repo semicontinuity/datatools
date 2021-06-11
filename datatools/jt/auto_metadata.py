11378adb0a0439d33dbda8514fafa61b18990fce
semicontinuity <igor.a.karpov@gmail.com>
2021-06-11 17:48:24 +0300

    jtng: auto-detect timestamp format

--- datatools/jt/auto_metadata.py
+++ datatools/jt/auto_metadata.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 from typing import Dict, Set
 
 from datatools.json.util import is_primitive, dataclass_from_dict
+from datatools.util.text_util import infer_timestamp_format
 
 
 @dataclass
@@ -23,12 +24,17 @@ def infer_metadata(data, raw_metadata):
         return {k: dataclass_from_dict(ColumnMetadata, v) for k, v in raw_columns_metadata.items()}
 
     column_metadata_map = defaultdict(lambda: ColumnMetadata(set(), {}))
-    infer_metadata0(data, column_metadata_map)
+    timestamp_formats = infer_metadata0(data, column_metadata_map)
+    good_timestamp_formats = [(k, v) for k, v in timestamp_formats.items() if v != '']
+    if len(good_timestamp_formats) == 1:
+        raw_metadata['timestamp_field'] = good_timestamp_formats[0][0]
+        raw_metadata['timestamp_format'] = good_timestamp_formats[0][1]
     infer_metadata1(data, column_metadata_map)
     return column_metadata_map
 
 
 def infer_metadata0(data, column_metadata_map):
+    timestamp_formats = {}
     for record in data:
         for key, value in record.items():
             column_metadata = column_metadata_map[key]
@@ -46,8 +52,16 @@ def infer_metadata0(data, column_metadata_map):
                     column_metadata.type = 'any'
             else:
                 column_metadata.type = 'primitive'
-                if type(value) is str and '\n' in value:
-                    column_metadata.multiline = True
+                if type(value) is str:
+                    current_ts_format = timestamp_formats.get(key)
+                    if '\n' in value:
+                        column_metadata.multiline = True
+                        timestamp_formats[key] = ''
+                    elif current_ts_format != '':
+                        format, _ = infer_timestamp_format(value)
+                        if format != current_ts_format:
+                            timestamp_formats[key] = format if current_ts_format is None else ''
+    return timestamp_formats
 
 
 def infer_metadata1(data, column_metadata_map):
