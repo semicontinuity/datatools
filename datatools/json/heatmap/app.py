fd7a57c0362296ad78e91543c78ae47729510b4d
semicontinuity <igor.a.karpov@gmail.com>
2022-07-13 14:11:00 +0300

    datatools.json.heatmap.app: workaround for empty lanes

--- datatools/json/heatmap/app.py
+++ datatools/json/heatmap/app.py
@@ -8,13 +8,23 @@ from datatools.tui.terminal import ansi_attr_bytes
 from .mono_sixel_buffer import MonoSixelBuffer
 
 
-def paint_points(points: List[Sequence[int]], width: int, height: int):
+# NB:
+# Sixel painting is strange: if a lane of zero sixels is to be painted, default background is not applied!
+# As a workaround, inverse picture is generated: most sixels are 1, and points set sixel to 0.
+# It is also possible to paint in two passes: background first, then points - but it is actually more work.
+def paint_points(points: List[Sequence[int]], width: int, height: int, x_min, y_min, x_max, y_max):
+    x_unit = (x_max - x_min) / width
+    y_unit = (y_max - y_min) / height
+
     sixel_buffer = MonoSixelBuffer(width, height)
     for x, y in points:
-        sixel_buffer.add_point(x, y)
+        image_x = int((x - x_min) / x_unit)
+        image_y = int((y - y_min) / y_unit)
+        sixel_buffer.add_point(image_x, image_y)
 
-    os.write(1, ansi_attr_bytes(b'40')) # black BG
+    os.write(1, ansi_attr_bytes(b'107'))  # bright white BG: color for points
     os.write(1, sixel_buffer.to_bytes())
+    os.write(1, ansi_attr_bytes(b'0'))    # reset attrs
 
 
 def load_points():
@@ -27,10 +37,21 @@ def load_points():
         sys.exit(0x3F)
 
 
-if __name__ == "__main__":
-    if len(sys.argv) < 3:
-        print("Arguments: <width> <height>", file=sys.stderr)
+def main():
+    if len(sys.argv) < 7:
+        print("Arguments: <width> <height> <x-left> <y-left> <x-right> <y-right>", file=sys.stderr)
         sys.exit(0x3F)
+
     width = int(sys.argv[1])
     height = int(sys.argv[2])
-    paint_points(load_points(), width, height)
+
+    x_left = int(sys.argv[3])
+    y_left = int(sys.argv[4])
+    x_right = int(sys.argv[5])
+    y_right = int(sys.argv[6])
+
+    paint_points(load_points(), width, height, x_left, y_left, x_right, y_right)
+
+
+if __name__ == "__main__":
+    main()
--- datatools/json/heatmap/mono_sixel_buffer.py
+++ datatools/json/heatmap/mono_sixel_buffer.py
@@ -10,12 +10,13 @@ class MonoSixelBuffer:
     def __init__(self, width: int, height: int):
         self.__width = width
         self.__height = height
-        self.__sixels = [bytearray(width) for _ in range((height + 5) // 6)]
+        self.__sixels = [bytearray(b'?' * width) for _ in range((height + 5) // 6)]
 
     def to_bytes(self) -> bytes:
         buffer = bytearray()
-        sixel_append_start_cmd(buffer)
-        sixel_append_set_color_register_cmd(buffer, 0, 0, 99, 0)
+
+        sixel_append_start_cmd(buffer, p2=2)
+        sixel_append_set_color_register_cmd(buffer, 0, 0, 0, 0)
         sixel_append_use_color(buffer, 0)
 
         for lane in self.__sixels:
@@ -24,9 +25,10 @@ class MonoSixelBuffer:
             sixel_append_lf(buffer)
 
         sixel_append_stop_cmd(buffer)
+
         return buffer
 
     def add_point(self, x: int, y: int):
         lane_idx = y // 6
         lane_bit = y % 6
-        self.__sixels[lane_idx][x] |= (1 << lane_bit)
+        self.__sixels[lane_idx][x] &= ~(1 << lane_bit)
