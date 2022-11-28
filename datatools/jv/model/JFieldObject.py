77ad287a5e12dc26f462b60d311bfde9dc3de357
semicontinuity <igor.a.karpov@gmail.com>
2022-11-28 10:23:45 +0300

    jv: first take on parsing objects with object fields (2)

--- datatools/jv/model/JFieldObject.py
+++ datatools/jv/model/JFieldObject.py
@@ -1,20 +1,23 @@
+from typing import List
+
 from datatools.jv.model.JFieldObjectEnd import JFieldObjectEnd
 from datatools.jv.model.JFieldObjectStart import JFieldObjectStart
 from datatools.jv.model.JObjectField import JObjectField
-from datatools.jv.model.JObjectFields import JObjectFields
 
 
 class JFieldObject(JObjectField):
     start: JFieldObjectStart
-    fields: JObjectFields
+    fields: List[JObjectField]
     end: JFieldObjectEnd
 
-    def __init__(self, name: str, indent=0, has_trailing_comma=False) -> None:
+    def __init__(self, name: str, fields: List[JObjectField], indent=0, has_trailing_comma=False) -> None:
         super().__init__(name, indent, has_trailing_comma)
         self.start = JFieldObjectStart(name, indent)
+        self.fields = fields
         self.end = JFieldObjectEnd(indent, has_trailing_comma)
 
     def elements(self):
         yield self.start
-        yield from self.fields.elements()
+        for field in self.fields:
+            yield from field.elements()
         yield self.end
--- datatools/jv/model/__init__.py
+++ datatools/jv/model/__init__.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from datatools.jv.model.JBoolean import JBoolean
 from datatools.jv.model.JElement import JElement
 from datatools.jv.model.JFieldBoolean import JFieldBoolean
@@ -31,9 +33,7 @@ def build_object_field_model(k: str, v, indent, has_trailing_comma) -> JObjectFi
     elif type(v) is bool:
         return JFieldBoolean(v, k, indent, has_trailing_comma)
     elif type(v) is dict:
-        model = JFieldObject(k, indent, has_trailing_comma)
-        model.fields = build_object_fields_model(v, indent + INDENT)
-        return model
+        return JFieldObject(k, build_object_fields_model2(v, indent + INDENT), indent, has_trailing_comma)
 
 
 def build_object_fields_model(j, indent) -> JObjectFields:
@@ -46,6 +46,16 @@ def build_object_fields_model(j, indent) -> JObjectFields:
         model.fields.append(build_object_field_model(k, v, indent, i < size - 1))
         i += 1
     return model
+
+
+def build_object_fields_model2(j, indent) -> List[JObjectField]:
+    fields = []
+    i = 0
+    size = len(j)
+    for k, v in j.items():
+        fields.append(build_object_field_model(k, v, indent, i < size - 1))
+        i += 1
+    return fields
 
 
 def build_model(v, indent=0, has_trailing_comma=False) -> JElement:
