d1b2085f2e34c3a45587aad61f11d87843768cd1
semicontinuity <igor.a.karpov@gmail.com>
2022-12-09 16:05:47 +0300

    fstree: initial model

--- datatools/fstree/app.py
+++ datatools/fstree/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
+from pathlib import Path
 
 from datatools.fstree.applet import Applet
 from datatools.fstree.fs_tree_document import FsTreeDocument
@@ -9,8 +10,21 @@ from datatools.tui.picotui_patch import patch_picotui
 from datatools.tui.picotui_util import *
 from datatools.tui.terminal import screen_size_or_default
 from datatools.tui.treeview.grid import GridContext, grid
-from datatools.tui.treeview.treedocument import TreeDocument
 from datatools.tui.tui_fd import infer_fd_tui
+
+
+def make_model(path: Path, parent: FsFolder = None, indent: int = 0, last: bool = True):
+    folder = FsFolder(path.name, indent, last)
+
+    children = []
+    sub_paths = [sub_path for sub_path in path.iterdir() if sub_path.is_dir()]
+
+    for i, sub_path in enumerate(sub_paths):
+        children.append(make_model(sub_path, folder, indent + 2, i == len(sub_paths) - 1))
+
+    folder.parent = parent
+    folder.set_elements(children)
+    return folder
 
 
 def sample_model():
@@ -33,11 +47,11 @@ def sample_model():
     return folder
 
 
-def make_fs_tree_applet():
+def make_fs_tree_applet(root: str):
     screen_width, screen_height = screen_size_or_default()
     grid_context = GridContext(0, 0, screen_width, screen_height)
-    folder = sample_model()
-    document = FsTreeDocument(folder, "/path")
+    folder = make_model(Path(root))
+    document = FsTreeDocument(folder, root)
     document.layout()
     document.optimize_layout(screen_height)
     document.layout()
@@ -68,8 +82,10 @@ def main():
 
 
 def do_main():
-    return run(lambda: make_fs_tree_applet().run())
+    return run(lambda: make_fs_tree_applet(sys.argv[1]).run())
 
 
 if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        sys.exit(1)
     main()
