127e01c84066bc9447b973d32c48470d5e77b8ce
semicontinuity <igor.a.karpov@gmail.com>
2022-12-10 11:14:45 +0300

    fstree: adapt DynamicDialog

--- datatools/fstree/dynamic_dialog.py
+++ datatools/fstree/dynamic_dialog.py
@@ -1,35 +1,35 @@
-from picotui.widgets import Dialog
+from picotui.editor import Editor
 
 from datatools.tui.picotui_util import *
 
 
-class DynamicDialog(Dialog):
-    def __init__(self, screen_height, x, y, w=0, h=0):
-        super().__init__(x, y, w, h)
+class DynamicDialog:
+    def __init__(self, screen_height: int, target: Editor):
         self.screen_height = screen_height
+        self.target = target
 
-    def request_height(self, max_child_h):
-        old_h = self.h
-        self.h = min(max_child_h, self.screen_height)
-        if old_h > self.h:
-            self.clear_box(self.x, self.y + self.h, self.w, old_h - self.h)
+    def request_height(self, height):
+        old_h = self.target.height
+        self.target.height = min(height, self.screen_height)
+        if old_h > self.target.height:
+            self.clear_box(self.target.x, self.target.y + self.target.height, self.target.width, old_h - self.target.height)
 
-        overshoot = max(self.y + self.h - self.screen_height, 0)
+        overshoot = max(self.target.y + self.target.height - self.screen_height, 0)
         if overshoot > 0:
             Screen.goto(0, self.screen_height - 1)
             for _ in range(overshoot):
                 Screen.wr('\r\n')
-            self.y -= overshoot
+            self.target.y -= overshoot
 
     def clear(self):
-        self.attr_reset()
-        self.clear_box(self.x, self.y, self.w, self.h)
+        Screen.attr_reset()
+        self.clear_box(self.target.x, self.target.y, self.target.width, self.target.height)
 
     def clear_box(self, left, top, width, height):
         # "\x1b[%s;%s;%s;%s$z" doesn't work
         s = b" " * width
         bottom = top + height
         while top < bottom:
-            self.goto(left, top)
-            self.clear_num_pos(width)
+            Screen.goto(left, top)
+            Screen.clear_num_pos(width)
             top += 1
