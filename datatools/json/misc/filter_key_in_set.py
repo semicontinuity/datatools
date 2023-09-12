40138525b528ec3b76e003aa8bfac0c2ff5babe4
semicontinuity <igor.a.karpov@gmail.com>
2023-09-12 17:43:54 +0300

    filter_key_in_set.py

---
+++ datatools/json/misc/filter_key_in_set.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+############################################################
+# Filters a stream of json lines.
+# Arguments: <KEY> <FILE with KEY values, one per line>
+#
+# Json line matches, if it has key KEY with value,
+# contained in the FILE with a list of values.
+############################################################
+import sys, json
+
+
+def load_lines(file_name: str):
+    values = set()
+    with open(file_name) as f:
+        for line in f:
+            values.add(line.rstrip('\n'))
+    return values
+
+
+def main():
+    key = sys.argv[1]
+    values = load_lines(sys.argv[2])
+
+    for line in sys.stdin:
+        o = json.loads(line)
+        value = o.get(key)
+        if value in values:
+            print(json.dumps(o))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print('Arguments: <KEY> <file with KEY values, one per line>')
+    else:
+        main()
