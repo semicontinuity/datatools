d8ec165a7dfddce25946aa3455ab7d44b3c9b85d
semicontinuity <igor.a.karpov@gmail.com>
2023-10-17 09:17:00 +0300

    jtaa refresh

--- datatools/tui/coloring.py
+++ datatools/tui/coloring.py
@@ -1,8 +1,9 @@
 # For dark theme
 from typing import Optional, Tuple
+import mmh3
 
 
-def hash_code(s):
+def hash_code0(s):
     """ Consistent hash """
     if type(s) is int:
         return s
@@ -12,6 +13,16 @@ def hash_code(s):
     for c in s:
         hh = (31 * hh + ord(c)) % 4294967296
     return hh
+
+
+def hash_code(s):
+    """ Consistent hash """
+    if type(s) is int:
+        return s
+    if type(s) is bool:
+        return int(s)
+    # 16, 24, 46, 64, 77, 80, 81, 94, 101, 109, 115, 117, 119, 132, 134, 137, 139, 174, 180, 213, 222, 237
+    return mmh3.hash(s, 237)
 
 
 def hash_to_rgb(h, offset=128, scale=1):
