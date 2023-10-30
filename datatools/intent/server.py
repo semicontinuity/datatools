02ac79e54bda61a7f2e6a75312efe3ff8a8ecf13
semicontinuity <igor.a.karpov@gmail.com>
2023-10-30 23:45:27 +0300

    intent server (2)

--- datatools/intent/server.py
+++ datatools/intent/server.py
@@ -9,21 +9,59 @@ Environment variables for the launched process are specified in "x-env" HTTP hea
 as JSON with the format {"var":"value"..}.
 For working directory of the process, the value of the variable PWD is used.
 """
-
+import json
+import os
+import subprocess
 from http.server import BaseHTTPRequestHandler
 from http.server import HTTPServer
+from typing import Dict, List
+
+
+def exe(self, cwd: str, args: List[str], vars: Dict[str, str], stdin: bytes = None):
+    env = os.environ | vars
+    proc = subprocess.Popen(
+        args,
+        cwd=cwd,
+        env=env,
+        stdin=subprocess.PIPE,
+        stdout=subprocess.STDOUT,
+        stderr=subprocess.DEVNULL,
+    )
+    out, err = proc.communicate(stdin)
+    if proc.returncode != 0:
+        raise Exception(proc.returncode)
+    return out.decode()
 
 
 class Server(BaseHTTPRequestHandler):
     def do_GET(self):
-        for k, v in self.headers.items():
-            print(k, v)
         self.respond(200, 'application/json', b'{}')
 
     def do_POST(self):
         content_len = int(self.headers.get('Content-Length'))
         post_body = self.rfile.read(content_len)
-        self.respond(200, 'application/json', post_body)
+        payload = json.loads(post_body)
+
+        env_str = self.headers['x-env']
+        if env_str is None:
+            self.respond(400, 'application/json', b'{"error":"x-env header missing"}')
+            return
+
+        env = json.loads(env_str)
+        cwd = env['CWD']
+        if cwd is None:
+            self.respond(400, 'application/json', b'{"error":"CWD variable in x-env header missing"}')
+            return
+
+        del env['CWD']
+
+        if 'PWD' in env:
+            del env['PWD']
+
+        # exe(
+        #     cwd
+        # )
+        self.respond(200, 'application/json', json.dumps(payload).encode('utf-8'))
 
     def respond(self, status, content_type, content):
         self.send_response(status)
