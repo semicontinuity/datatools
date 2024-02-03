d0907482d2062742d2b0b497ab4ef62816a03faa
semicontinuity <igor.a.karpov@gmail.com>
2024-02-03 16:53:09 +0300

    ChooseItemGrid (1)

--- datatools/tui/app/choose.py
+++ datatools/tui/app/choose.py
@@ -1,5 +1,7 @@
 import sys
-from typing import Callable, Any
+from typing import Callable, Any, List
+
+from picotui.defs import KEY_ESC
 
 from datatools.fstree.exit_codes_mapping import KEYS_TO_EXIT_CODES
 from datatools.tui.grid_base import WGridBase
@@ -12,10 +14,11 @@ from datatools.tui.picotui_util import *
 
 class ChooseItemGrid(WGridBase):
 
-    def __init__(self, x: int, y: int, width: int, height: int, y_top_offset, y_bottom_offset, interactive=True):
+    def __init__(self, data: List[str], x: int, y: int, width: int, height: int, y_top_offset, y_bottom_offset,
+                 interactive=True):
         super().__init__(x, y, width, height, y_top_offset, y_bottom_offset, interactive)
-        self.set_lines(['A', 'A', 'A'])
-        self.total_lines = 3
+        self.set_lines(data)
+        self.total_lines = len(data)
 
     def clear(self):
         self.attr_reset()
@@ -25,22 +28,16 @@ class ChooseItemGrid(WGridBase):
         self.redraw_content()
 
     def render_line(self, line, is_under_cursor):
-        return 'AAA' if is_under_cursor else 'aaa'
+        l = self.content[line]
+        l += ' ' * (self.width - len(l))
+        if is_under_cursor:
+            return '\x1b[7m' + l + '\x1b[27m'
+        else:
+            return l
 
     def handle_edit_key(self, key):
         if key in KEYS_TO_EXIT_CODES:
             return key
-
-
-def grid() -> ChooseItemGrid:
-    return ChooseItemGrid(
-        10,
-        10,
-        20,
-        3,
-        0,
-        0
-    )
 
 
 def run_dialog(dialog_supplier: Callable[[int, int, int, int], Any]):
@@ -53,7 +50,11 @@ def run_dialog(dialog_supplier: Callable[[int, int, int, int], Any]):
         cursor_y, cursor_x = cursor_position()
 
         dialog = dialog_supplier(screen_height, screen_width, cursor_y, cursor_x)
-        dialog.loop()
+        res = dialog.loop()
+        if res == KEY_ESC:
+            return None
+        else:
+            return dialog.content[dialog.cur_line]
 
     finally:
         Screen.attr_reset()
@@ -66,11 +67,18 @@ def run_dialog(dialog_supplier: Callable[[int, int, int, int], Any]):
 
 
 def main():
-    sys.exit(
-        run_dialog(
-            lambda h, w, y, x: grid()
-        )
-    )
+    data = [line.rstrip() for line in sys.stdin]
+    if len(data) < 1:
+        return
+    max_line_length = max((len(s) for s in data))
+
+    res = run_dialog(
+        lambda h, w, y, x: ChooseItemGrid(data, x, y, min(max_line_length, w - x), min(h - y, len(data)), 0, 0))
+
+    if res is None:
+        sys.exit(1)
+    else:
+        print(res)
 
 
 if __name__ == "__main__":
