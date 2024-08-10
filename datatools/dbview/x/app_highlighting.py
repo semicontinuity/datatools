b648dccbf6425f8c5bc2749adf61b29dd25eb93b
semicontinuity <igor.a.karpov@gmail.com>
2024-08-10 12:33:16 +0300

    dbview enhancements - pk strings

--- datatools/dbview/x/app_highlighting.py
+++ datatools/dbview/x/app_highlighting.py
@@ -1,6 +1,7 @@
 from typing import Hashable, Dict, Any, Sequence
 
 from datatools.jv.highlighting.console import ConsoleHighlighting
+from datatools.jv.model import JString
 from datatools.tui.buffer.abstract_buffer_writer import AbstractBufferWriter
 from datatools.tui.coloring import hash_to_rgb
 from datatools.tui.treeview.rich_text import Style
@@ -11,11 +12,16 @@ class AppHighlighting(ConsoleHighlighting):
     def __init__(self, references: Dict[str, Any]) -> None:
         self.references = references
 
+    def for_string(self, node: JString) -> Style:
+        return Style(AbstractBufferWriter.MASK_UNDERLINED if self.is_fk(node.path()) else 0, (64, 160, 192))
+
     def for_field_label(self, label: str, indent: int, path: Sequence[Hashable]) -> Style:
         # return (Style(AbstractBufferWriter.MASK_BOLD, hash_to_rgb(hash_code(label))))
         # return (Style(AbstractBufferWriter.MASK_BOLD, hash_to_rgb(indent * 731593 ^ indent * 1363)))
         # return (Style(AbstractBufferWriter.MASK_BOLD, hash_to_rgb(0)))
         indent = len(path) * 2
-        is_foreign_key = len(path) == 4 and path[0:3] == ['ENTITY', 'data', 'self'] and path[3] in self.references
-        return Style(AbstractBufferWriter.MASK_BOLD | (AbstractBufferWriter.MASK_UNDERLINED if is_foreign_key else 0),
+        return Style(AbstractBufferWriter.MASK_BOLD | (AbstractBufferWriter.MASK_UNDERLINED if self.is_fk(path) else 0),
                      hash_to_rgb(0xc03b << (indent % 15)))
+
+    def is_fk(self, path):
+        return len(path) == 4 and path[0:3] == ['ENTITY', 'data', 'self'] and path[3] in self.references
--- datatools/jv/highlighting/console.py
+++ datatools/jv/highlighting/console.py
@@ -1,7 +1,7 @@
 from typing import Hashable, List
 
 from datatools.jv.highlighting.highlighting import Highlighting
-from datatools.jv.model import JElement
+from datatools.jv.model import JString
 from datatools.tui.buffer.abstract_buffer_writer import AbstractBufferWriter
 from datatools.tui.coloring import hash_to_rgb
 from datatools.tui.treeview.rich_text import Style
@@ -16,7 +16,7 @@ class ConsoleHighlighting(Highlighting):
 
     def for_number(self) -> Style: return Style(AbstractBufferWriter.MASK_BOLD, (192, 96, 96))
 
-    def for_string(self, node: JElement) -> Style: return Style(0, (64, 160, 192))
+    def for_string(self, node: JString) -> Style: return Style(0, (64, 160, 192))
 
     def for_field_label(self, label: str, indent: int, path: List[Hashable]) -> Style:
         # return (Style(AbstractBufferWriter.MASK_BOLD, hash_to_rgb(hash_code(label))))
