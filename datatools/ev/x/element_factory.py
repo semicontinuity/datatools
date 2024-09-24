4ef04a8ffad3eb91962e66be77dc3bdde34a50e7
semicontinuity <igor.a.karpov@gmail.com>
2024-09-24 21:01:24 +0300

    ev: basic nav

--- datatools/ev/x/element_factory.py
+++ datatools/ev/x/element_factory.py
@@ -1,6 +1,9 @@
 from typing import Optional
 
+from picotui.defs import KEY_ENTER
+
 from datatools.ev.x.concepts import Concepts
+from datatools.ev.x.types import RestEntity
 from datatools.jv.model.JElement import JElement
 from datatools.jv.model.JNumber import JNumber
 from datatools.jv.model.factory import JElementFactory
@@ -10,18 +13,27 @@ from datatools.tui.treeview.rich_text import Style
 
 class MyElementFactory(JElementFactory):
     concepts: Concepts
+    concept: str
 
     def __init__(self, concepts: Concepts):
         super().__init__(None)
         self.concepts = concepts
 
     class JMyNumber(JNumber):
+        def set_concept(self, concept: str):
+            self.concept = concept
+            return self
+
         def value_style(self):
             return Style(AbstractBufferWriter.MASK_ITALIC | AbstractBufferWriter.MASK_UNDERLINED, (192, 96, 96))
+
+        def handle_key(self, key: str):
+            if key == KEY_ENTER:
+                return RestEntity(self.concept, self.value)
 
     def make_number_element(self, v, k, parent: Optional[JElement] = None):
         concept = self.concepts.find_concept(parent.path() + [k])
         if concept:
-            return MyElementFactory.JMyNumber(v, k)
+            return MyElementFactory.JMyNumber(v, k).set_concept(concept)
         else:
             return JNumber(v, k)
