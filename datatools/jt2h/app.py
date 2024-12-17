b0f00ea032dfffa2e0cea57fee34067837423ca2
semicontinuity <igor.a.karpov@gmail.com>
2024-12-17 23:08:08 +0300

    refactor jt2h app

--- datatools/jt2h/app.py
+++ datatools/jt2h/app.py
@@ -2,18 +2,14 @@
 import json
 import sys
 from collections import defaultdict
-from datetime import datetime
 
-from datatools.jt2h.ats_channels import channel_deleted, channel_created, channel_color, channel_event_type, channel_use
 from datatools.jt2h.column_renderer_colored import ColumnRendererColored
-from datatools.jt2h.column_renderer_custom import ColumnRendererCustom
-from datatools.jt2h.column_renderer_entities_lifecycle import ColumnRendererEntitiesLifecycle
 from datatools.jt2h.json_node_delegate_yaml2_css import JSON_NODE_DELEGATE_YAML_CSS
 from datatools.jt2h.json_node_helper_css import JSON_NODE_HELPER_CSS
 from datatools.jt2h.log_node import LogNode
 from datatools.jt2h.log_node_js import LOG_NODE_JS
 from datatools.jt2h.page_node_css import PAGE_NODE_CSS
-from util.html.elements import span, html, head, style, script, body
+from util.html.elements import html, head, style, script, body, title
 from util.html.page_node import PageNode
 
 
@@ -25,26 +21,29 @@ def data():
 
 
 def main():
-    j = data()
-    print(page_node(j, column_renderers(j)))
+    print(page_node_auto(data()))
 
 
-def page_node(j, r):
-    return page_node_for(LogNode(j, r))
+def page_node_auto(j, script_text: str = LOG_NODE_JS, title_str: str = None):
+    return page_node(j, column_renderers_auto(j), script_text, title_str)
 
 
-def page_node_for(log_node):
+def page_node(j, renderers, script_text: str = LOG_NODE_JS, title_str: str = None):
+    return page_node_for(LogNode(j, renderers), script_text, title_str)
+
+
+def page_node_for(log_node, script_text: str, title_str: str):
     return PageNode(
         html(
-
             head(
+                title(title_str),
                 style(
                     PAGE_NODE_CSS,
                     log_node.css(),
                     JSON_NODE_HELPER_CSS,
                     JSON_NODE_DELEGATE_YAML_CSS
                 ),
-                script(LOG_NODE_JS)
+                script(script_text) if script_text else None
             ),
 
             body(
@@ -54,7 +53,7 @@ def page_node_for(log_node):
     )
 
 
-def column_renderers(j):
+def column_renderers_auto(j):
     column_counts = defaultdict(int)
 
     for row in j:
---
+++ datatools/jupyter/__init__.py
@@ -0,0 +1,34 @@
+import uuid
+
+from IPython.display import HTML
+
+from datatools.jt2h.app import page_node_auto
+from datatools.jt2h.app_json_page import page_node
+
+
+def JSON_TAB(data, title=None, tab_name=None):
+    return render(str(page_node(data, title)), tab_name, title)
+
+
+def JSON_TABLE_TAB(data, title=None, tab_name=None):
+    return render(str(page_node_auto(data, script_text=None, title_str=title)), tab_name, title)
+
+
+def render(html, tab_name, title):
+    a_id = uuid.uuid4()
+    return HTML(
+        """
+        <a id="{a_id}">{text}</a>
+
+        <script type="text/javascript">
+          document.getElementById("{a_id}").onclick = function() {{
+              window.open("", "{name}").document.body.innerHTML = `{html}`;
+          }};
+        </script>
+        """.format(
+            a_id=a_id,
+            html=html,
+            text='Show in new tab' if title is None else title,
+            name=a_id if tab_name is None else tab_name,
+        )
+    )
