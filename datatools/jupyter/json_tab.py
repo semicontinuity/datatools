93fa5e3576befb83ff85479e24182592cc13d99d
semicontinuity <igor.a.karpov@gmail.com>
2024-12-17 19:18:42 +0300

    JSON_TAB

--- datatools/jupyter/json_tab.py
+++ datatools/jupyter/json_tab.py
@@ -1,21 +1,26 @@
+import uuid
+
 from IPython.display import HTML
 from datatools.jt2h.app_json_page import page_node
 
 
 def JSON_TAB(data, title=None, tab_name=None):
     html = str(page_node(data, title))
+    a_id = uuid.uuid4()
+
     return HTML(
         """
+        <a id="{a_id}">{text}</a>
+
         <script type="text/javascript">
-        function www() {{
-            var win = window.open("", "{name}");
-            win.document.body.innerHTML = `{html}`;
-        }};
+          document.getElementById("{a_id}").onclick = function() {{
+              window.open("", "{name}").document.body.innerHTML = `{html}`;
+          }};
         </script>
-        <a onclick="www()">{text}</a>
         """.format(
+            a_id=a_id,
             html=html,
             text='Show in new tab' if title is None else title,
-            name=str(hash(html)) if tab_name is None else tab_name,
+            name=a_id if tab_name is None else tab_name,
         )
     )
