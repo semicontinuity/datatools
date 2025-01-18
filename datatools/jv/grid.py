8263455ce44bbd98c9288a4ac29ccf403ad2f3cb
semicontinuity <igor.a.karpov@gmail.com>
2025-01-18 15:16:14 +0300

    jv += search (0)

--- datatools/jv/grid.py
+++ datatools/jv/grid.py
@@ -1,9 +1,19 @@
 from datatools.tui.picotui_keys import KEY_INSERT
 from datatools.tui.treeview.grid import WGrid
+from datatools.tui.treeview.treedocument import TreeDocument
 from datatools.util.object_exporter import ObjectExporter
 
 
 class JGrid(WGrid):
+    search_str: str
+
+    def __init__(self, x: int, y: int, width, height, document: TreeDocument, interactive=True):
+        super().__init__(x, y, width, height, document, interactive)
+        self.search_str = ""
+
+    def handle_cursor_keys(self, key):
+        self.search_str = ""
+        return super().handle_cursor_keys(key)
 
     def handle_edit_key(self, key):
         if key == KEY_INSERT:
@@ -15,8 +25,22 @@ class JGrid(WGrid):
         else:
             node = self.document.selected_value_node(self.cur_line)
             result = node.handle_key(key)
-            if result is None:
-                return super().handle_edit_key(key)
-            else:
+            if result is not None:
                 return result
+            else:
+                result = super().handle_edit_key(key)
+                if result:
+                    return result
+                if type(key) is bytes:
+                    self.handle_typed_key(key)
 
+    def handle_typed_key(self, key):
+        self.search_str += key.decode("utf-8")
+        # line = self.search()
+        # if line is not None:
+        #     content_height = self.height - 3
+        #     if line >= self.top_line + content_height:
+        #         delta = line - self.cur_line
+        #         self.top_line += delta
+        #     self.cur_line = line
+        #     self.redraw_lines(self.top_line, content_height)
