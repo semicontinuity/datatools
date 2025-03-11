8ee80aa141d0648443b8c22588c7ee4bd15b3d7f
semicontinuity <igor.a.karpov@gmail.com>
2025-03-11 10:28:06 +0300

    sent entity as multipart - cleanup

--- datatools/ev/entity_transfer/__init__.py
+++ datatools/ev/entity_transfer/__init__.py
@@ -2,15 +2,19 @@ import os
 from pathlib import Path
 
 
-def create_ctx_reference_chain(ctx, referring_path: Path, referenced_path: Path):
+def create_ctx_reference_chain(ctx: Path, referring_path: Path, referenced_path: Path):
     for part in ctx.parts:
         referenced_path = referenced_path / part
         if not referenced_path.exists():
             return False
 
         referring_path = referring_path / part
+
+        # print('mkdir', referring_path)
         os.makedirs(referring_path, exist_ok=True)
-        os.chmod(str(referring_path), os.stat(str(referenced_path)).st_mode)
+        st_mode = os.stat(str(referenced_path)).st_mode
+        # print('st_mode', st_mode)
+        os.chmod(str(referring_path), st_mode)
 
         ref = os.path.relpath(
             path=str(referenced_path),
@@ -18,8 +22,14 @@ def create_ctx_reference_chain(ctx, referring_path: Path, referenced_path: Path)
         )
 
         context_pointer = referring_path / '._'
+        # print('context_pointer', context_pointer)
         if not context_pointer.exists():
+            # print('symlink', ref, context_pointer)
             os.symlink(ref, context_pointer)
+        # else:
+            # print('exists')
+
+        # print()
 
     return True
 
--- datatools/intent/handler_send_entity.py
+++ datatools/intent/handler_send_entity.py
@@ -1,7 +1,8 @@
 import os
+from pathlib import Path
 
 from datatools.dbview.x.util.pg_query import query_from_yaml
-from datatools.ev.entity_transfer import write_entity_parts, create_context_pointers_chain
+from datatools.ev.entity_transfer import write_entity_parts, create_ctx_reference_chain
 from datatools.intent.target_folder import default_folder
 from datatools.json.util import to_jsonisable
 
@@ -11,10 +12,19 @@ class HandlerSendEntity:
 
     def send_entity(self, realm_ctx: str, realm_ctx_dir: str, query_str: str, content: bytes = None):
         query = query_from_yaml(query_str)
+        entity_realm_path = make_entity_realm_path(query)
 
+        ctx_dir = realm_ctx_dir.removesuffix('/' + realm_ctx)
         created_realm_folder = f"{self.folder}/{realm_ctx}"
-        create_context_pointers_chain(created_realm_folder, realm_ctx_dir)
-        entity_path = f"{created_realm_folder}/{make_entity_realm_path(query)}"
+
+        entity_path = f"{created_realm_folder}/{entity_realm_path}"
+        chain = Path(realm_ctx) / entity_realm_path
+
+        # create as much as possible...
+        create_ctx_reference_chain(
+            ctx=chain, referring_path=Path(self.folder), referenced_path=Path(ctx_dir)
+        )
+
         os.makedirs(entity_path, exist_ok=True)
 
         write_entity_parts(entity_path, query_str, content)
