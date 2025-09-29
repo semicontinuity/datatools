5819193483ec05beacc6981276ab86f6570240fc
semicontinuity <igor.a.karpov@gmail.com>
2025-09-29 15:35:25 +0300

    entity graph updates

--- datatools/ev/x/graph/renderer.py
+++ datatools/ev/x/graph/renderer.py
@@ -1,3 +1,4 @@
+import json
 import os
 import sys
 from pathlib import Path
@@ -13,20 +14,58 @@ def make_graph(root: Path):
     dot.attr(rankdir=(RANKDIR or 'LR'))
     
     # Iterate through all entries in the root directory
-    for folder in root.iterdir():
-        # Check if it's a directory
-        if folder.is_dir():
-            folder_name = folder.name
-            # Iterate through all entries in the subfolder
-            for subfolder in folder.iterdir():
-                # Check if it's a directory
-                if subfolder.is_dir():
-                    subfolder_name = subfolder.name
-                    if subfolder_name != '._':
-                        # Create a node with name "folder:subfolder"
-                        node_id = f"{folder_name}:{subfolder_name}"
-                        dot.node(node_id, node_id)
-    
+    for entity_type_folder in root.iterdir():
+        if not entity_type_folder.is_dir():
+            continue
+
+        entity_type_folder_name = entity_type_folder.name
+        if entity_type_folder_name[0].islower():
+            # Now entity types start with uppercase letter; e.g. `AtsChannel`
+            continue
+
+        for entity_ids_slug_folder in entity_type_folder.iterdir():
+            # Check if it's a directory
+            if entity_ids_slug_folder.is_dir():
+                entity_ids_slug_folder_name = entity_ids_slug_folder.name
+                # Create a node with name "folder:subfolder"
+                if entity_ids_slug_folder_name == '._':
+                    continue
+
+                file_path = entity_ids_slug_folder / '.entity_ids'
+                # print(file_path, file=sys.stderr)
+
+                if file_path.exists():
+                    with open(file_path, 'r') as file:
+                        entity_ids = json.load(file)
+                        # print(entity_ids, file=sys.stderr)
+                else:
+                    entity_ids = [entity_ids_slug_folder_name]
+
+                for entity_id in entity_ids:
+                    node_id = f"{entity_type_folder_name}#{entity_id}"
+                    # dot.node(node_id, node_id)
+                    dot.node(node_id)
+
+                for manifestation_folder in entity_ids_slug_folder.iterdir():
+                    print(manifestation_folder, file=sys.stderr)
+                    file_path = manifestation_folder / '.referred_entity_ids'
+                    if file_path.exists():
+                        with open(file_path, 'r') as file:
+                            referred_entities = json.load(file)
+                            for referred_entity_type, referred_entity_ids in referred_entities.items():
+                                for referred_entity_id in referred_entity_ids:
+                                    for entity_id in entity_ids:
+
+                                        if '@' in entity_id or '@' in referred_entity_id:
+                                            continue
+                                        if '/' in entity_id or '/' in referred_entity_id:
+                                            continue
+
+                                        dot.edge(
+                                            f"{entity_type_folder_name}#{entity_id}",
+                                            f"{referred_entity_type}#{referred_entity_id}",
+                                        )
+
     return dot
 
 
