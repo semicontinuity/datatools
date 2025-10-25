0eb60eb6a106dcff0c5b0cdd3ade4c472048c551
semicontinuity <igor.a.karpov@gmail.com>
2025-10-25 20:00:52 +0300

    EntitiesGraph

--- datatools/ev/x/graph/renderer.py
+++ datatools/ev/x/graph/renderer.py
@@ -10,9 +10,14 @@ RANKDIR = os.environ.get('RANKDIR')
 
 
 class EntitiesGraph:
-    def make_graph(self, root: Path):
-        dot = Digraph('Entities', format='png')
-        dot.attr(rankdir=(RANKDIR or 'LR'))
+
+    def __init__(self) -> None:
+        self.dot = Digraph('Entities', format='png')
+        self.dot.attr(rankdir=(RANKDIR or 'LR'))
+
+        self.nodes = set()
+
+    def fill_nodes(self, root: Path):
 
         # Iterate through all entries in the root directory
         for entity_type_folder in root.iterdir():
@@ -37,7 +42,7 @@ class EntitiesGraph:
                     if entity_ids_file_path.exists():
                         if os.environ.get('MULTI') == '0':
                             # skip multi-entities
-                            print('skip', entity_ids_slug_folder)
+                            # print('skip', entity_ids_slug_folder, file=sys.stderr)
                             continue
 
                         with open(entity_ids_file_path, 'r') as file:
@@ -48,7 +53,40 @@ class EntitiesGraph:
                     for entity_id in entity_ids:
                         node_id = f"{entity_type_folder_name}#{entity_id}"
                         # dot.node(node_id, node_id)
-                        dot.node(node_id)
+                        self.dot.node(node_id)
+
+    def make_graph(self, root: Path):
+
+        # Iterate through all entries in the root directory
+        for entity_type_folder in root.iterdir():
+            if not entity_type_folder.is_dir():
+                continue
+
+            entity_type_folder_name = entity_type_folder.name
+            if entity_type_folder_name[0].islower():
+                # Now entity types start with uppercase letter; e.g. `AtsChannel`
+                continue
+
+            for entity_ids_slug_folder in entity_type_folder.iterdir():
+                # Check if it's a directory
+                if entity_ids_slug_folder.is_dir():
+                    entity_ids_slug_folder_name = entity_ids_slug_folder.name
+                    # Create a node with name "folder:subfolder"
+                    if entity_ids_slug_folder_name == '._':
+                        continue
+
+                    entity_ids_file_path = entity_ids_slug_folder / '.entity_ids'
+
+                    if entity_ids_file_path.exists():
+                        if os.environ.get('MULTI') == '0':
+                            # skip multi-entities
+                            # print('skip', entity_ids_slug_folder, file=sys.stderr)
+                            continue
+
+                        with open(entity_ids_file_path, 'r') as file:
+                            entity_ids = json.load(file)
+                    else:
+                        entity_ids = [entity_ids_slug_folder_name]
 
                     for manifestation_folder in entity_ids_slug_folder.iterdir():
                         print(manifestation_folder, file=sys.stderr)
@@ -65,21 +103,22 @@ class EntitiesGraph:
                                             if '/' in entity_id or '/' in referred_entity_id:
                                                 continue
 
-                                            dot.edge(
+                                            self.dot.edge(
                                                 f"{entity_type_folder_name}#{entity_id}",
                                                 f"{referred_entity_type}#{referred_entity_id}",
                                             )
 
-        return dot
-
 
 def main():
     folder = Path(os.environ['PWD'])
-    dot = EntitiesGraph().make_graph(folder)
+    graph = EntitiesGraph()
+    graph.fill_nodes(folder)
+    graph.make_graph(folder)
+
     if os.environ.get('SVG'):
-        sys.stdout.buffer.write(dot.pipe(format='svg'))
+        sys.stdout.buffer.write(graph.dot.pipe(format='svg'))
     else:
-        print(dot.source)
+        print(graph.dot.source)
 
 
 if __name__ == '__main__':
