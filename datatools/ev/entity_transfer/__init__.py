fa19e361ddf3d50e6f65de21acbd5fc633f6b000
semicontinuity <igor.a.karpov@gmail.com>
2025-12-13 09:32:14 +0300

    entity_reference_chain.py

--- datatools/ev/entity_transfer/__init__.py
+++ datatools/ev/entity_transfer/__init__.py
@@ -1,35 +1,4 @@
-import os
 from pathlib import Path
-
-
-def create_ctx_reference_chain(ctx: Path, referring_path: Path, referenced_path: Path):
-    for part in ctx.parts:
-        referenced_path = referenced_path / part
-        if not referenced_path.exists():
-            return False
-
-        referring_path = referring_path / part
-
-        create_ctx_reference(referring_path, referenced_path)
-
-    return True
-
-
-def create_ctx_reference(referring_path: Path, referenced_path: Path):
-    # print('mkdir', referring_path)
-    os.makedirs(referring_path, exist_ok=True)
-    st_mode = os.stat(str(referenced_path)).st_mode
-    # print('st_mode', st_mode)
-    os.chmod(str(referring_path), st_mode)
-    ref = os.path.relpath(
-        path=str(referenced_path),
-        start=str(referring_path),
-    )
-    context_pointer = referring_path / '._'
-    # print('context_pointer', context_pointer)
-    if not context_pointer.exists():
-        # print('symlink', ref, context_pointer)
-        os.symlink(ref, context_pointer)
 
 
 def write_entity_parts_b0(entity_path, query_str, content: bytes):
---
+++ datatools/ev/util/__init__.py
@@ -0,0 +1,1 @@
+CONTEXT_POINTER = '._'
