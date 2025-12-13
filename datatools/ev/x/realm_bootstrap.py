13425c29ef2a3e3649274608a8ff8f5f2a51bd28
semicontinuity <igor.a.karpov@gmail.com>
2025-12-13 10:29:03 +0300

    use resolved realm path

--- datatools/ev/x/realm_bootstrap.py
+++ datatools/ev/x/realm_bootstrap.py
@@ -1,22 +1,19 @@
-import json
 import os
-from os.path import relpath
-from typing import Dict
+from pathlib import Path
 
-from datatools.ev.app_support import run_app
-from datatools.ev.app_types import Realm
-from datatools.ev.x.pg.entity_resolver import initial_entity_ref
-from datatools.ev.x.pg.pg_data_source import PgDataSource
-from datatools.ev.x.pg.realm_pg import RealmPg
+from datatools.ev.util.entity_reference_chain import ctx_to_resources
 
 
 def get_realm_ctx(props):
-    return relpath(
-        get_realm_ctx_dir(props),
-        start=props.get('CTX_DIR')
+    return str(
+        ctx_to_resources(ctx=Path(get_ctx(props)).parent, ctx_dir=Path(props['CTX_DIR']))[-1]
     )
 
 
 def get_realm_ctx_dir(props):
-    return os.path.abspath(props.get('CTX_DIR') + '/' + (props.get('CTX_BASE') or props.get('CTX')) + '/..')
+    return os.path.abspath(props.get('CTX_DIR') + '/' + get_ctx(props) + '/..')
+
+
+def get_ctx(props):
+    return props.get('CTX_BASE') or props.get('CTX')
 
