9f0459c18fc1e6f5922944018d03c0568781227e
semicontinuity <igor.a.karpov@gmail.com>
2026-04-25 14:39:33 +0300

    intents: rework (ACTIONS dict)

--- datatools/intent/handler_text_plain.py
+++ datatools/intent/handler_text_plain.py
@@ -1,19 +1,24 @@
 import email.message
 from typing import AnyStr
 
-from datatools.intent import local_file_url
+from datatools.intent.action_open_in_browser import do_open_in_browser
 from datatools.intent.action_set_clipboard import do_set_clipboard
-from datatools.intent.targets import browse_new_tab, write_temp_file_name
 from datatools.tui.popup_selector import choose
+
+ACTIONS = {
+    'set-clipboard': {
+        'text': 'Copy to Clipboard',
+        'handler': do_set_clipboard,
+    },
+    'open-in-browser': {
+        'text': 'Open in Browser',
+        'handler': do_open_in_browser,
+    },
+}
 
 
 def process_text_plain(headers: email.message.Message, post_body: AnyStr):
-    match choose(["Copy to Clipboard", "Open in Browser"], 'Choose'):
-        case 0:
-            do_set_clipboard(headers, post_body)
-        case 1:
-            browse_new_tab(
-                local_file_url(
-                    write_temp_file_name(post_body, '.txt', headers.get('X-Title'))
-                )
-            )
+    actions = list(ACTIONS.values())
+    choice = choose([a['text'] for a in actions], 'Choose')
+    if choice is not None:
+        actions[choice]['handler'](headers, post_body)
